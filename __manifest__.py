155b6f916508ed3a3661d8d8ea80ef716b598131
Marco <marcocaaguilar@gmail.com>
2019-02-18 13:49:37 -0600

    Primer commit modulo openacademy

---
+++ __manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "openacademy",
+
+    'summary': """Modulo creado con el curso de openacademy""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Marco",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Pruebas',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/openacademy.xml',
+        'views/partner.xml',
+        'security/ir.model.access.csv'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'application': True,
+    'installable': True
+}
